5db767c77aacf952bbb2e91c5b839a711b53c1e4
hoaipeter <peterhoai0803@gmail.com>
2017-10-20 14:25:00 +0700

    Update VR Samples

--- WorldView-master/Assets/VRSampleScenes/Scripts/Flyer/FlyerMovementController.cs
+++ WorldView-master/Assets/VRSampleScenes/Scripts/Flyer/FlyerMovementController.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Security.Cryptography.X509Certificates;
 using UnityEngine.UI;
 using UnityEngine.VR;
 using VRStandardAssets.Common;
@@ -72,7 +71,7 @@ namespace VRStandardAssets.Flyer
             while (m_IsGameRunning)
             {
                 // Set the target marker position to a point forward of the camera multiplied by the distance from the camera.
-                Quaternion headRotation = InputTracking.GetLocalRotation (VRNode.Head);
+                Quaternion headRotation = UnityEngine.XR.InputTracking.GetLocalRotation (UnityEngine.XR.XRNode.Head);
                 m_TargetMarker.position = m_Camera.position + (headRotation * Vector3.forward) * m_DistanceFromCamera;
 
                 // Move the camera container forward.
